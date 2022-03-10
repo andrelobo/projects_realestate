d789c00e1d7f36b4186f4e40b85a777b11090cd0
andrelobo <loboandre@hotmail.com>
2022-03-10 14:41:27 -0400

    fix:using yarn now and bayout api settings

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+    'x-rapidapi-key': '67d5af03e7mshfafeff08510bbc4p1ad20cjsnee3edeb325c8',
     },
   });
     
